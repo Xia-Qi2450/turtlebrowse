f2df8b860584fcdfc996311b787c6a924425e4df
Ethan Lee Qi Yang <ingstudiosofficial@gmail.com>
2026-07-22 12:45:08 +0800

    fix: remove unused import

--- app/src/main/java/dev/ingstudios/turtlebrowse/windows/NewProfileWindow.java
+++ app/src/main/java/dev/ingstudios/turtlebrowse/windows/NewProfileWindow.java
@@ -2,7 +2,6 @@ package dev.ingstudios.turtlebrowse.windows;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
